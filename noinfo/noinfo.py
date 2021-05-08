b267727fef58415c7095c59054ce9d1c3f4c5c75
Vexed <gh.vexed@gmail.com>
2021-05-08 16:05:20 +0100

    because i would totally do that on a private repo

--- noinfo/noinfo.py
+++
@@ -1,8 +0,0 @@
-from redbot.core import commands
-
-class NoInfo(commands.Cog):
-    """This is a local cog, used for testing."""
-    @commands.command()
-    async def noinfocom(self, ctx):
-        """This is a local cog, used for testing."""
-        await ctx.send("This is a local cog, used for testing.")
