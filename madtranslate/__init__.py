9c0bf5c18e13df5b42e81e645b00797bebbce365
Vexed01 <vex@vexcodes.com>
2022-01-04 10:32:11 +0000

    "remove" madtranslate

--- madtranslate/__init__.py
+++ madtranslate/__init__.py
@@ -1,6 +1,4 @@
-from redbot.core.bot import Red
-from .madtranslate import MadTranslate
+from redbot.core.errors import CogLoadError
 
-
-def setup(bot: Red):
-    bot.add_cog(MadTranslate(bot))
+def setup(bot):
+    raise CogLoadError("MadTranslate has been moved to my main repo at https://github.com/Vexed01/Vex-Cogs\n\nPlease use the new link to install it.")
